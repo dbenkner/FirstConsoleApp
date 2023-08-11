a4c5403de5673d796b6bed404cf6200d5709bac7
David Benkner <d.benkner@gmail.com>
2023-08-10 21:22:30 -0400

    intial commit

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FirstConsoleApp
+{
+    /// <summary>
+    /// First Console App code
+    /// </summary>
+    /// 
+    internal class Program
+    {
+        /// <summary>
+        /// First code
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // print supportive message
+            Console.WriteLine("David Benkner");
+            Console.WriteLine("Eddie Sotto");
+        }
+    }
+}
